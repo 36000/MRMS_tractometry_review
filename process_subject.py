9f6960be702b60a49424c0a1dfb49d56c5420ceb
36000 <jk6.28@outlook.com>
2024-03-26 15:20:16 -0700

    update code after reviews

--- process_subject.py
+++ process_subject.py
@@ -4,8 +4,6 @@ from dipy.data import fetch_hbn
 
 
 cwd = os.getcwd()
-reco_path = cwd + "/hbn_bids/HBN/derivatives/reco/sub-NDARAV554TP2/"
-os.makedirs(reco_path, exist_ok=True)
 afq_path = cwd + "/hbn_bids/HBN/derivatives/afq/sub-NDARAV554TP2/"
 os.makedirs(afq_path, exist_ok=True)
 qsi_dwi_path = cwd + "/hbn_bids/HBN/derivatives/qsiprep/sub-NDARAV554TP2/ses-HBNsiteRU/dwi/"
@@ -25,21 +23,5 @@ myafq = ParticipantAFQ(
         "prob_threshold": 0.10,
         "parallel_segmentation": {"engine": "serial"}})
 #myafq.cmd_outputs(cmd="rm", dependent_on="recog")
-myafq.export_all()
-
-# copy results into reco folder, then clear everything
-# dependent on bundle recognition
-myafq.cmd_outputs(cmd="cp", suffix=reco_path)
-myafq = ParticipantAFQ(
-    qsi_base_path + ".nii.gz",
-    qsi_base_path + ".bval",
-    qsi_base_path + ".bvec",
-    reco_path,
-    tracking_paras={"n_seeds": 2},
-    segmentation_params={
-        "seg_algo": "reco80",
-        "save_intermediates": cwd + "/reco_models/",
-        "parallel_segmentation": {"engine": "serial"}})
-myafq.cmd_outputs(cmd="rm", dependent_on="recog")
 myafq.export_all()
 
